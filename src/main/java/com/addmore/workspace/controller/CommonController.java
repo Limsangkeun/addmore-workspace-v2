cdc8fc7fed87b425977bf4eeb6dc2cb282e810df
Limsangkeun <dlatkdrms301@gmail.com>
2023-03-20 23:38:22 +0900

    add vueJS Default Setup

--- src/main/java/com/addmore/workspace/controller/CommonController.java
+++ src/main/java/com/addmore/workspace/controller/CommonController.java
@@ -1,7 +1,12 @@
 package com.addmore.workspace.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -10,5 +15,12 @@ public class CommonController {
     @RequestMapping
     public String main() {
         return "index";
+    }
+
+    @RequestMapping("/api/test")
+    public ResponseEntity<Map<String, Object>> testApi() {
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("name", "sangkeun");
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
     }
 }
