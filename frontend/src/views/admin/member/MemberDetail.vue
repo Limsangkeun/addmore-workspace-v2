2c1b56623fadac2cf2a200ea7c1ab48d0164cf6d
Limsangkeun <dlatkdrms301@naver.com>
2023-04-07 18:01:00 +0900

    직원 관리 작업 중

--- frontend/src/views/admin/member/MemberDetail.vue
+++ frontend/src/views/admin/member/MemberDetail.vue
@@ -1,15 +1,47 @@
 <script setup>
-const emits = defineEmits(['goList']);
-const goList = () => {
-  emits('goList', 'list');
-}
 </script>
 
 <template>
   <div class="grid">
     <div class="col-12">
-      <h6>유저 상세</h6>
-      <Button @click="goList"></Button>
+      <div class="p-fluid">
+        <div class="field grid">
+          <label for="name3" class="col-12 mb-2 md:col-2 md:mb-0">아이디</label>
+          <div class="col-12 md:col-10">
+            <InputText id="name3" type="text" />
+          </div>
+        </div>
+        <div class="field grid">
+          <label for="name3" class="col-12 mb-2 md:col-2 md:mb-0">비밀번호</label>
+          <div class="col-12 md:col-10">
+            <InputText id="name3" type="text" class=""/>
+          </div>
+        </div>
+        <div class="field grid">
+          <label for="name3" class="col-12 mb-2 md:col-2 md:mb-0">이름</label>
+          <div class="col-12 md:col-10">
+            <InputText id="name3" type="text" />
+          </div>
+        </div>
+        <div class="field grid">
+          <label for="name3" class="col-12 mb-2 md:col-2 md:mb-0">부서</label>
+          <div class="col-12 md:col-10">
+            <InputText id="name3" type="text" />
+          </div>
+        </div>
+        <div class="field grid">
+          <label for="name3" class="col-12 mb-2 md:col-2 md:mb-0">생년월일</label>
+          <div class="col-12 md:col-10">
+            <Calendar id="name3" type="text" />
+          </div>
+        </div>
+        <div class="field grid">
+          <label for="name3" class="col-12 mb-2 md:col-2 md:mb-0">입사일</label>
+          <div class="col-12 md:col-10">
+            <Calendar id="name3" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- frontend/src/views/admin/member/MemberList.vue
+++ frontend/src/views/admin/member/MemberList.vue
@@ -1,42 +1,44 @@
 <script setup>
-const emits = defineEmits(['goDetail']);
-
-const goDetail = (model) => {
-  emits('goDetail',  'detail', model);
-}
+import MemberDetail from "@/views/admin/member/MemberDetail.vue";
 </script>
 
 <template>
+  <div class="card flex-1">
   <div class="grid">
-    <div class="col-12">
-      <div class="card">
-        <DataTable
-            dataKey="id"
-            :paginator="true"
-            :rows="20"
-            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-            :rowsPerPageOptions="[5, 10, 25]"
-            currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
-            responsiveLayout="scroll"
-        >
-          <template #header>
-            <div class="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
-              <h5 class="m-0 font-bold">직원현황</h5>
-              <div class="my-2 flex flex-row">
-                <span class="block mt-2 mr-2 md:mt-0 p-input-icon-left">
-                  <i class="pi pi-search" />
-                  <InputText placeholder="Search..." />
-                </span>
-                <Button label="Search" icon="pi pi-search" class="mr-2"/>
-                <Button label="New" icon="pi pi-plus" class="p-button-success mr-2" @click="goDetail"/>
-                <Button label="Delete" icon="pi pi-trash" class="p-button-danger"/>
-              </div>
-            </div>
-          </template>
-          <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
-        </DataTable>
+    <div class="col-6">
+      <div class="flex flex-row md:justify-content-between md:align-items-center mb-3">
+      <h5 class="m-0 font-bold">직원현황</h5>
+      <div class="my-2 flex flex-row">
+            <span class="block mt-2 mr-2 md:mt-0 p-input-icon-left">
+              <i class="pi pi-search"/>
+              <InputText placeholder="Search..."/>
+            </span>
       </div>
+      </div>
+      <DataTable
+          dataKey="id"
+          :paginator="true"
+          :rows="20"
+          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+          :rowsPerPageOptions="[5, 10, 25]"
+          currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+          responsiveLayout="scroll"
+          style="min-height: 500px"
+      >
+        <Column selectionMode="multiple" headerStyle="width: 3rem"></Column>
+      </DataTable>
     </div>
+    <div class="col-6">
+      <TabView>
+        <TabPanel header="상세정보">
+          <MemberDetail></MemberDetail>
+        </TabPanel>
+        <TabPanel header="권한">
+
+        </TabPanel>
+      </TabView>
+    </div>
+  </div>
   </div>
 </template>
 
